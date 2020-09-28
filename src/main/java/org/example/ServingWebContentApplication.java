62069921a9506aac8aff59742cd1e29a9818263c
Oleh_Strykun <alik49410@gmail.com>
2020-09-28 21:42:03 +0300

    add iml ot

--- src/main/java/org/example/ServingWebContentApplication.java
+++ src/main/java/org/example/ServingWebContentApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class ServingWebContentApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ServingWebContentApplication.class, args);
     }
