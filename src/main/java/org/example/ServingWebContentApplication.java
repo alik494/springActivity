0216945ef8c9ca68e901c23730baf53bdb1734ef
Oleh_Strykun <alik49410@gmail.com>
2020-09-28 22:31:16 +0300

    1

--- src/main/java/org/example/ServingWebContentApplication.java
+++ src/main/java/org/example/ServingWebContentApplication.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class ServingWebContentApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(ServingWebContentApplication.class, args);
     }
